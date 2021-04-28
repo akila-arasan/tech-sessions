4206830882228e34d6e89e8e77625fe484a8d3fa
Akilandeswari Durai Arasan -X (aduraiar - ENQUERO INC at Cisco) <aduraiar@cisco.com>
2021-04-29 00:22:18 +0530

    feat(Setup): Added routing config for the session

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SlidesComponent } from './pages/slides/slides/slides.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'sd-session-docs',
+    pathMatch: 'full'
+  },
+  {
+    path: 'sd-session-docs',
+    children: [
+      {
+        path: ':id',
+        component: SlidesComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
