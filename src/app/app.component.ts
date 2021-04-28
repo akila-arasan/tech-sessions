7b62995a17382976c8b9d451c718546e776f87f8
Akilandeswari Durai Arasan -X (aduraiar - ENQUERO INC at Cisco) <aduraiar@cisco.com>
2021-04-29 01:07:57 +0530

    feat(Slides): Added Intro Slides

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'SystemDesignSession';
+  title = 'Tech-sessions';
 
   keyMap = {
     previous: 'b',
